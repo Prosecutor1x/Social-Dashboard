5c88c373d882e261b9b4d5faefc7724717864001
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-12-20 23:33:54 +0530

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,41 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        quicksand: ["Quicksand", "Sans-Serif"],
+        monsterrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        xs: "10px",
+        sx: "12px",
+        sm: "14px",
+        base: "16px",
+        lg: "18px",
+        xl: "20px",
+        "1.5xl": "24px",
+        "2xl": "32px",
+        "3xl": "40px",
+        "4xl": "48px",
+        "5xl": "60px",
+        "6xl": "72px",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1108px",
+        xl: "1224px",
+        "2xl": "1370px",
+      },
+      colors: {
+        theme: "#045b63",
+        font2: "#44476A",
+        font1: "#045b63",
       },
     },
   },
   plugins: [],
 }
+
+
 export default config
