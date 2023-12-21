e1335abd07e5e57704207cf03d9f27fff853ede0
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-12-21 19:07:00 +0530

    navbar and footer

--- src/components/header/navbar/main-navbar.tsx
+++ src/components/header/navbar/main-navbar.tsx
@@ -1,26 +1,21 @@
-import Link from "next/link";
-import React from "react";
+import { useAuth } from "@/context/auth-context";
+import { useUserContext } from "@/context/user-context";
 
 import {
-  IconButton,
+  Avatar,
   Menu,
   MenuButton,
   MenuDivider,
+  MenuGroup,
   MenuItem,
   MenuList,
 } from "@chakra-ui/react";
-import {
-  AddIcon,
-  ChevronDownIcon,
-  EditIcon,
-  ExternalLinkIcon,
-  HamburgerIcon,
-  RepeatIcon,
-} from "@chakra-ui/icons";
-import { useAuth } from "@/context/auth-context";
+import Link from "next/link";
+import React from "react";
 
-export default function Navbar() {
-  const { logout } = useAuth();
+function Navbar() {
+  const { authUser, logout } = useAuth();
+  const { user } = useUserContext();
 
   const handleLogOut = async () => {
     try {
@@ -31,9 +26,52 @@ export default function Navbar() {
   };
 
   return (
-    <>
-      NAvbar
-      <button onClick={handleLogOut}>Logout</button>
-    </>
+    <div className="bg-[#3b42a1] py-2 px-10 font-quicksand font-semibold sticky top-0  ">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center  gap-10 ">
+          <img src="favicon.ico" className="h-12" />
+          <h1 className="text-">FrameLux</h1>
+        </div>
+
+        {authUser != null ? (
+          <Menu>
+            <MenuButton>
+              <Avatar
+                name={user?.displayName as string}
+                src={user?.photoURL as string}
+              />
+            </MenuButton>
+            <MenuList zIndex={40} className="font-semibold font-quicksand">
+              <MenuItem as="a" href="/">
+                Home
+              </MenuItem>
+              <MenuItem as="a" href="/dashboard">
+                Dashboard
+              </MenuItem>
+              <MenuItem as="a" href="/profile">
+                Profile{" "}
+              </MenuItem>
+              <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        ) : (
+          <div className="flex gap-2">
+            <Link href="/signup">
+              <button className="bg-black text-white p-2 rounded-lg px-8 active:scale-95">
+                Register
+              </button>
+            </Link>
+            <Link href="/login">
+              <button className="bg-black text-white p-2 rounded-lg px-8 active:scale-95">
+                Log In
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+      {/* <button onClick={handleLogOut}>Logout</button> */}
+    </div>
   );
 }
+
+export default Navbar;
